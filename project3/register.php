ada5df3628b0830d742e812b093f8d9f7e39c4c9
vincentwu8 <98859765+vincentwu8@users.noreply.github.com>
2023-04-22 16:46:11 -0700

    Add files via upload

---
+++ project3/register.php
@@ -0,0 +1,51 @@
+<?php
+
+if (empty($_POST["username"])){
+    die("username id required");
+}
+
+if(empty($_POST["first-name"])){
+    die("firstname required");
+}
+
+if(empty($_POST["last-name"])){
+    die("lastname required");
+}
+
+if(! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+    die("Valid email is required");
+}
+
+if(strlen($_POST["password"]) < 8){
+    die("Password must be at least 8 characters");
+}
+
+if($_POST["password"] !== $_POST["confirm-password"]){
+    die("passwords must match");
+}
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO user(user_id, first_name, last_name, email_address, DOB, city, gender, password)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+$stmt = $mysqli->stmt_init();
+
+if( ! $stmt->prepare($sql)){
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("ssssssss",
+                $_POST["username"],
+                $_POST["first-name"],
+                $_POST["last-name"],
+                $_POST["email"],
+                $_POST["date-of-birth"],
+                $_POST["hometown"],
+                $_POST["gender"],
+                $_POST["password"]);
+
+$stmt->execute();
+
+echo "signup good";
+#print_r($_POST);
